cbc0191aa7e946bbdfa3f36e3bc5c63e29ed11ec
llj098 <llj098@gmail.com>
2013-05-28 23:53:23 +0800

    add time init

--- Arch/init.sh
+++ Arch/init.sh
@@ -1,3 +1,13 @@
 
+
+
+
+sudo timedatectl set-local-rtc 0
+sudo timedatectl set-timezone Asia/Chongqing
+
+sudo pacman -S ntp
+sudo ntpd -qg
+sudo hwclock -w
+
 sudo pacman -S ttf-inconsolata dina-font
 
