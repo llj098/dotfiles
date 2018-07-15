0c3915e595fc9fa6b6d026088522fc674e6354f0
LJ <llj098>
2018-07-15 11:29:00 +0800

    add font conf

---
+++ Arch/fonts.sh
@@ -0,0 +1,4 @@
+pacman -S ttf-roboto noto-fonts noto-fonts-cjk adobe-source-han-sans-cn-fonts adobe-source-han-serif-cn-fonts ttf-dejavu
+
+sudo cp /etc/fonts/fonts.conf /etc/fonts/fonts.bak.conf
+sudo cp fonts.android.conf /etc/fonts/fonts.conf
