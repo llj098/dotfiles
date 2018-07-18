daa152e286b1faf2d86a6ac50d63170219f22a3f
llj098 <llj098@gmail.com>
2018-07-18 15:32:01 +0800

    update font

--- Arch/fonts.sh
+++ Arch/fonts.sh
@@ -1,4 +1,6 @@
-pacman -S ttf-roboto noto-fonts noto-fonts-cjk adobe-source-han-sans-cn-fonts adobe-source-han-serif-cn-fonts ttf-dejavu
+sudo pacman -S ttf-roboto noto-fonts noto-fonts-cjk adobe-source-han-sans-cn-fonts adobe-source-han-serif-cn-fonts ttf-dejavu
 
 sudo cp /etc/fonts/fonts.conf /etc/fonts/fonts.bak.conf
 sudo cp fonts.android.conf /etc/fonts/fonts.conf
+sudo cp ../font/* /usr/share/fonts/TTF/
+fc-cache -f -v 
