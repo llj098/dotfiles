b57f6c1298ba37f019bca2e774593b64e5c2d50a
liulijin <llj098@gmail.com>
2026-09-05 16:17:12 +0800

    fix: restore user proxy environment

---
+++ home/.config/user-env.sh
@@ -0,0 +1,8 @@
+# Local HTTP/HTTPS proxy for interactive and login shells.
+export http_proxy=http://127.0.0.1:7890
+export https_proxy=http://127.0.0.1:7890
+export HTTP_PROXY=http://127.0.0.1:7890
+export HTTPS_PROXY=http://127.0.0.1:7890
+export no_proxy=localhost,127.0.0.1,::1
+export NO_PROXY=localhost,127.0.0.1,::1
+unset all_proxy ALL_PROXY
