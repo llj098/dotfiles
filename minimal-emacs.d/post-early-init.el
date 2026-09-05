1121aee4723b79d4dd64fa7032efc8b5d8adcbdb
liulijin <llj098@gmail.com>
2026-09-05 18:25:35 +0800

    perf(emacs): use TUNA package mirrors

--- minimal-emacs.d/post-early-init.el
+++ minimal-emacs.d/post-early-init.el
@@ -8,3 +8,8 @@
 ;; Evil 1.15.0 from NonGNU relies on `evil-mode-buffers', which Emacs 31 no
 ;; longer creates.  Use the current MELPA build containing the Emacs 31 fix.
 (setq package-pinned-packages '((evil . "melpa")))
+
+(setq package-archives
+      '(("gnu"    . "https://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
+        ("nongnu" . "https://mirrors.tuna.tsinghua.edu.cn/elpa/nongnu/")
+        ("melpa"  . "https://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")))
